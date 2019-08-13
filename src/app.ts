62886baa68f9aa551fb5041039aa1efae58682dd
Benjamin Mark <mquadrant@gmail.com>
2019-08-13 14:08:08 +0100

    added cross origin resource middleware

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,14 @@ import express,{Request,Response,NextFunction} from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import cors from 'cors';
 
 import indexRouter from './routes/index';
 import usersRouter from './routes/users';
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
