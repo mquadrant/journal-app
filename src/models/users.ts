9abc36045c98a3f46770de1592613401a654f1ff
Benjamin Mark <mquadrant@gmail.com>
2019-08-13 11:55:54 +0100

    created user schema model

---
+++ src/models/users.ts
@@ -0,0 +1,41 @@
+import mongoose,{Schema,Document} from 'mongoose';
+
+export interface IUser extends Document{
+    firstName:string;
+    lastName:string;
+    email:string;
+    password:string;
+}
+
+const userSchema:Schema = new Schema({
+    first_name: {
+        type: String,
+        required: [true, "A user must have first name"],
+        trim: true,
+        maxlength: [
+            40,
+            "A user name must have less or equal to 40 characters",
+        ],
+    },
+    last_name: {
+        type: String,
+        required:[true,"A user must have first name"],
+        trim: true,
+        maxlength: [
+            40,
+            "A user name must have less or equal to 40 characters",
+        ],
+    },
+    email: {
+        type: String,
+        required:[true,"A user must have email"],
+        trim: true,
+    },
+    password:{
+        type:String,
+        required:[true,"A user must have password"]
+    }
+});
+
+
+export default mongoose.model<IUser>('User',userSchema);
