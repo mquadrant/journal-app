4e30cac9a9dbe5baac4f40a823499af0c70db6a3
Benjamin Mark <mquadrant@gmail.com>
2019-08-14 16:59:25 +0100

    added update and delete route

--- src/routes/user/users.ts
+++ src/routes/user/users.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import User from './../../models/users'
+import {validateUserEdit} from './userValidation';
 
 const router = express.Router();
 
@@ -38,7 +39,9 @@ router.get('/:userId', async function(req, res, _next) {
     res.status(200).json({
       status: "success",
       data: {
-          user,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          email: user.email,
       }
     })
   }catch(error){
@@ -50,12 +53,52 @@ router.get('/:userId', async function(req, res, _next) {
   
 });
 /* PATCH user updating. */
-router.patch('/:userId', function(_req, res, _next) {
-  res.send('respond with a resource');
+router.patch('/:userId', async function(req, res, _next) {
+  const data = req.body;
+  const {error} = validateUserEdit(data);
+  if (error) {
+    // send a 422 error response if validation fails
+    res.status(422).json({
+      status: 'error',
+      message: 'Invalid request data',
+      data: data,
+    });
+  }else{
+    try{
+      const user = await User.findByIdAndUpdate(req.params.userId,data);
+      if(!user)throw new Error("User does not exist!");
+      res.status(201).json({
+        status: "success",
+        data: {
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+        }
+      })
+    }catch(error){
+      res.status(404).json({
+        status:"fail",
+        message: error
+      });
+    }
+  }
+
 });
-/* GET users listing. */
-router.delete('/:userId', function(_req, res, _next) {
-  res.send('respond with a resource');
+/* DELETE deleting a user by id. */
+router.delete('/:userId', function(req, res, _next) {
+  try{
+    const user = User.deleteOne({_id:req.params.userId});
+    if(!user)throw new Error("User doesn't exist!")
+    res.status(204).json({
+      status: "success"
+    })
+  }catch(error){
+    res.status(404).json({
+      status:"fail",
+      message: error
+    });
+  }
+  
 });
 
 export default router;
