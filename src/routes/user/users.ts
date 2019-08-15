0b7e4f33c598d551c2f6fae92b58c65609e76e28
Benjamin Mark <mquadrant@gmail.com>
2019-08-15 10:16:10 +0100

    refactored

--- src/routes/user/users.ts
+++ src/routes/user/users.ts
@@ -1,8 +1,6 @@
 import express from 'express';
 import bcrypt from 'bcrypt';
 import User from './../../models/users';
-import jwt from 'jsonwebtoken';
-import env from "./../../config"
 import {validateUserEdit,validateSignup} from './userValidation';
 
 const router = express.Router();
@@ -57,30 +55,7 @@ if(error){
   }
 }
 });
-/* POST user Logging in (Logging In). */
-router.post('/login', async function(req, res, _next) {
-  try{
-    const user = await User.findOne({email:req.body.email});
-    if (!user) throw new Error("Login Failed");
-      const isEqual = await bcrypt.compare(req.body.password, user.password);
-      if (!isEqual) throw new Error("Login Failed");
-      const token = jwt.sign(
-          { userId: user.id, email: user.email },
-          env.jwtSecret!,
-          {
-              expiresIn: "1h",
-          }
-      );
-      return res.status(200).json({
-        message:"successfully logged In",
-        userId: user.id,
-        token: token,
-        tokenExpiration: 1,
-      })
-  }catch(error){
-    throw error;
-  }
-});
+
 /* GET users listing. */
 router.get('/', async function(_req, res, _next) {
   try{
