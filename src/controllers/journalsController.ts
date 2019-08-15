6d93056effa08ffb5d35299132553d02a5d68cd3
Benjamin Mark <mquadrant@gmail.com>
2019-08-16 00:13:08 +0100

    added more error response

--- src/controllers/journalsController.ts
+++ src/controllers/journalsController.ts
@@ -4,25 +4,33 @@ import {validateJournal} from '../routes/journal/journalValidation';
 
 
 export const createJournal = async function (req:Request, res:Response, _next:NextFunction) {
-    const {error} = validateJournal(req.body);
+  const {userId} = req.body.userData
+  let clone = Object.assign({}, req.body);
+    delete clone.userData;
+    const {error} = validateJournal(clone);
     if(error){
     // send a 422 error response if validation fails
       return res.status(422).json({
         status: 'error',
         message: 'Invalid request data',
-        data: req.body,
+        data: clone
       });
     }else{
         try{
-            const journal = await new Journal(req.body).save();
-            if(!journal)throw new Error("Try again");
-            return res.status(201).json({
+            const journal = await new Journal({
+              ...req.body,
+              userId: userId
+            }).save();
+              return res.status(201).json({
                 status:'success',
                 message:"successfully saved",
                 data: journal
             })
         }catch(error){
-            throw error;
+          return res.status(500).json({
+            status:"fail",
+            error: "Something went wrong"
+          })
         }
         
 
@@ -30,24 +38,44 @@ export const createJournal = async function (req:Request, res:Response, _next:Ne
     }
 
 export const getAllUserJournals = async function(req:Request, res:Response, _next:NextFunction) {
+    const {userId} = req.body.userData;
     try{
-      const journals = await Journal.find({userId:req.params.userId});
-      if(!journals)throw new Error("No Journal is found");
-      res.status(200).json({
+      const journals = await Journal.find({userId:userId});
+      if(!journals){
+        return res.status(404).json({
+          status:"fail",
+          message: "No Journal found"
+        });
+      }
+      return res.status(200).json({
         status: "success",
         results: journals.length,
         data: journals
       })
     }catch(error){
-      throw error;
+      return res.status(404).json({
+        status:"fail",
+        message: error
+      });
     }
   } ;
 
 export const getSingleUserJournal = async function(req:Request, res:Response, _next:NextFunction) {
+  const {userId} = req.body.userData
     try{
         const journal = await Journal.findById(req.params.journalId);
-        if(!journal)throw new Error("Journal does not exist!");
-        else if(journal.userId !== req.params.userId)throw new Error("User can't read this journal");
+        if(!journal){
+          return res.status(404).json({
+            status:"fail",
+            message: "Journal does not exist"
+          });
+        }
+        else if(journal.userId.toString() !== userId){
+          return res.status(401).json({
+            status:"fail",
+            message: "Can not view this journal"
+          });
+        }
       else return res.status(200).json({
         status: "success",
         data: journal
@@ -61,20 +89,33 @@ export const getSingleUserJournal = async function(req:Request, res:Response, _n
   };
 
 export const updateJournal = async function(req:Request, res:Response, _next:NextFunction) {
-    const data = req.body;
-    const {error} = validateJournal(data);
+  const {userId} = req.body.userData
+  let clone = Object.assign({}, req.body);
+    delete clone.userData;
+    const {error} = validateJournal(clone);
     if (error) {
       // send a 422 error response if validation fails
       return res.status(422).json({
         status: 'error',
         message: 'Invalid request data',
-        data: data,
+        data: clone,
       });
     }else{
       try{
           const journalExist = await Journal.findById(req.params.journalId);
-          if(!journalExist||journalExist.userId !== req.params.userId)throw new Error("User or Journal does not exist!");
-        const journal = await Journal.findByIdAndUpdate(req.params.journalId,data);
+          if(!journalExist){
+            return res.status(401).json({
+              status:"fail",
+              message: "Journal doesn't exist!"
+            });
+          }
+          if(journalExist.userId.toString() !== userId){
+            return res.status(401).json({
+              status:"fail",
+              message: "User not authorized to update this journal"
+            });
+          }
+        const journal = await Journal.findByIdAndUpdate(req.params.journalId,clone,{new: true});
         return res.status(201).json({
           status: "success",
           data: journal
@@ -89,16 +130,27 @@ export const updateJournal = async function(req:Request, res:Response, _next:Nex
   };
 
 export const deleteJournal = async function(req:Request, res:Response, _next:NextFunction) {
+  const {userId} = req.body.userData
     try{
         const journal = await Journal.findOne({_id:req.params.journalId});
-        if(!journal)throw new Error("Journal doesn't exist!");
-        if(journal.userId !== req.params.userId) throw new Error ("Users not authorized to delete this journal");
+        if(!journal){
+          return res.status(401).json({
+            status:"fail",
+            message: "Journal doesn't exist!"
+          });
+        }
+        if(journal.userId.toString() !== userId) {
+          return res.status(401).json({
+            status:"fail",
+            message: "User not authorized to delete this journal"
+          });
+        }
         await Journal.deleteOne({_id:req.params.journalId});
-      res.status(204).json({
+        return res.status(204).json({
         status: "success"
       })
     }catch(error){
-      res.status(404).json({
+      return res.status(404).json({
         status:"fail",
         message: error
       });
