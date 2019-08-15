883054ee2c4c37164aa9274ff8e2ce9283002067
Benjamin Mark <mquadrant@gmail.com>
2019-08-15 10:16:36 +0100

    added app routes to index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,21 @@
 import express from 'express';
+import usersRouter from './user/users';
+import authRouter from './user/users';
+import journalRouter from './user/users';
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(_req, res, _next) {
   res.send({message:'Api homepage'})
 });
+
+/* USER Auth Routes. */
+router.use('/auth', authRouter);
+
+/* USER Routes. */
+router.use('/users', usersRouter);
+
+/* JOURNAL Routes. */
+router.use('/journals',journalRouter)
 
 export default router;
