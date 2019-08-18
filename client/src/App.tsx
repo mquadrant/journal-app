0bb4f5a72bc17e34b11ee5e261ab9340eed93bf4
Benjamin Mark <mquadrant@gmail.com>
2019-08-18 16:35:33 +0100

    app file clean up & structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+
+const AppRoutes = lazy(() => import('./routes/AppRoutes'));
+const LoginPage = lazy(() => import('./containers/Login/LoginPage'));
+const SignupPage = lazy(() => import('./containers/Signup/SignupPage'));
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Router>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route>
+              <Route path="/login" component={LoginPage} />
+              <Route path="/signup" component={SignupPage} />
+              <Route path="/app" component={AppRoutes} />
+            </Route>
+          </Switch>
+        </Suspense>
+      </Router>
+    </React.Fragment>
   );
 }
 
