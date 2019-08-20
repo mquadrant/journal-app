cc3ae868c759077d731743513bf9f093e2a3bc76
Benjamin Mark <mquadrant@gmail.com>
2019-08-20 01:50:32 +0100

    created header for dashboard

---
+++ client/src/components/Header2.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Logo2 from '../images/LogoAppsvg.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
+ 
+const DownArrow = <FontAwesomeIcon icon={faAngleDown} />
+
+const Header = styled.nav`
+    display: flex;
+    height: 63px;
+    background-color: #eae9f9;
+    align-items: center;
+    padding:8px 50px;
+
+    img.logo{
+        margin-right:auto;
+    }
+    ul.nav{
+        margin-left:auto;
+        display: flex;
+        flex-flow: row wrap;
+        justify-content: flex-end;
+        list-style:none;
+        li a {
+        color: #707070;
+        text-decoration: none;
+        margin: 0 1em;
+      }
+    }
+`;
+
+const Avatar = styled.div`
+    width: 25px;
+  height: 25px;
+  border: solid 1px #707070;
+`;
+
+interface Props {
+    
+}
+
+export default function Header2(): any {
+    return (
+        <Header>
+            <img src={Logo2} alt='Journal-Logo'/>
+            <ul className="nav">
+                <Avatar/>
+                <div>
+                {DownArrow}
+                </div>
+            </ul>
+        </Header>
+    )
+}
