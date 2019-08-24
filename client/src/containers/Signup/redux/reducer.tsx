58fff533758c35b4ec13139556e223baff3deb1a
Benjamin Mark <mquadrant@gmail.com>
2019-08-24 23:05:59 +0100

    created actions,types, and reducer for signup

---
+++ client/src/containers/Signup/redux/reducer.tsx
@@ -0,0 +1,33 @@
+import { SET_USER_CREDENTIAL, CREATE_USER_PENDING, CREATE_USER_ERROR} from './types';
+
+// set initial state 
+const initialState = {
+  credential:{},
+  error: null,
+  pending: false,
+};
+
+//reducer
+export default (state = initialState, action:any = {}) => {
+  switch (action.type) {
+    case SET_USER_CREDENTIAL:
+      return {
+        ...state,
+        credential : action.credential,
+        pending:false
+      };
+    case CREATE_USER_PENDING:
+      return {
+        ...state,
+        pending: true,
+      };
+    case CREATE_USER_ERROR:
+      return {
+        ...state,
+        error: action.error,
+        pending:false
+      };
+    default:
+      return state;
+  }
+};
