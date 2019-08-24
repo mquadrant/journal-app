cdf3a4b314d13347af75768976713c189a734016
Benjamin Mark <mquadrant@gmail.com>
2019-08-24 23:05:16 +0100

    added signup reducer to store

--- client/src/store/reducers.tsx
+++ client/src/store/reducers.tsx
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import login from '../containers/Login/redux/reducer';
+import signup from '../containers/Signup/redux/reducer';
 
 const rootReducer = combineReducers({
-  login
+  login,
+  signup
 });
 
 export default rootReducer;
