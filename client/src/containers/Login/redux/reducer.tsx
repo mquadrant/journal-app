11815d6c77aece6f479c78b0125bc7fceedee121
Benjamin Mark <mquadrant@gmail.com>
2019-08-24 13:06:30 +0100

    created reducer for login

---
+++ client/src/containers/Login/redux/reducer.tsx
@@ -0,0 +1,33 @@
+import { SET_CURRENT_USER, SET_PENDING, SET_ERROR} from './types';
+
+// set initial state 
+const initialState = {
+  isAuthenticated: false,
+  error: null,
+  pending: false,
+};
+
+//reducer
+export default (state = initialState, action:any = {}) => {
+  switch (action.type) {
+    case SET_CURRENT_USER:
+      return {
+        ...state,
+        isAuthenticated: action.token||false,
+        pending:false
+      };
+    case SET_PENDING:
+      return {
+        ...state,
+        pending: true,
+      };
+    case SET_ERROR:
+      return {
+        ...state,
+        error: action.error,
+        pending:false
+      };
+    default:
+      return state;
+  }
+};
