b471d9b91be6416317cd639102277c09b8b00473
Benjamin Mark <mquadrant@gmail.com>
2019-08-25 09:35:51 +0100

    made app routes protected

--- client/src/routes/AppRoutes.tsx
+++ client/src/routes/AppRoutes.tsx
@@ -2,6 +2,7 @@ import React, { Suspense, lazy } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import '../App.css';
 import Header2 from '../components/Header2';
+import Auth from '../utils/Auth';
 
 
 const DashboardPage = lazy(() => import("../containers/Dashboard/DashboardPage"));
@@ -17,8 +18,8 @@ export default function AppRoutes(): any {
       <Header2 />
       <Suspense fallback={<div>Loading...</div>}>
         <Switch>
-          <Route path="/app/dashboard" component={DashboardPage} />
-          <Route path="/app/journal/:cycleId/edit" component={JournalEditPage} />
+          <Route path="/app/dashboard" component={Auth(DashboardPage)} />
+          <Route path="/app/journal/:journalId/edit" component={Auth(JournalEditPage)} />
         </Switch>
       </Suspense>
     </React.Fragment>
