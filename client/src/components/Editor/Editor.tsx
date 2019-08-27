8e78e84c20134de7331c4f0186751a6d2dfbd5fa
Benjamin Mark <mquadrant@gmail.com>
2019-08-27 11:04:40 +0100

    add draft editor

--- client/src/components/Editor/Editor.tsx
+++ client/src/components/Editor/Editor.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import {Editor, EditorState} from 'draft-js';
 import './style.css';
 
-export default function MyEditor(): any {
+export default function MyEditor(updateEdit?:any): any {
   const [editorState, setEditorState]:any[] = React.useState(
     EditorState.createEmpty()
   );
-
+  const updateEdite = (editorState:any)=>{
+    console.log(editorState.getCurrentContent())
+    // setValues({
+    //   ...values,
+    //   body:editorState
+    // });
+  }
+  useEffect(() => {
+    updateEdite(editorState);
+  }, [editorState,updateEdit])
   return (
     <Editor
       editorState={editorState}
