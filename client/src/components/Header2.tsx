5e5d5be54728588371d13496d7773f52b27e88e9
Benjamin Mark <mquadrant@gmail.com>
2019-08-27 11:04:20 +0100

    add drop down and logout

--- client/src/components/Header2.tsx
+++ client/src/components/Header2.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React ,{createRef,useState,useEffect} from 'react';
 import styled from 'styled-components';
-import {Icon} from 'semantic-ui-react'
+import {Icon} from 'semantic-ui-react';
+import {Link } from 'react-router-dom';
 
 import Logo2 from '../images/DeepColorLogo.svg';
 import Image from '../images/avatar.png';
+import { setCurrentUser } from '../containers/Login/redux/actions';
+import store from '../store/store';
 
 const Header = styled.nav`
     display: flex;
@@ -19,6 +22,7 @@ const Header = styled.nav`
         margin-right:auto;
     }
     ul.nav{
+        position:relative;
         margin-left:auto;
         display: flex;
         flex-flow: row wrap;
@@ -30,6 +34,36 @@ const Header = styled.nav`
         margin: 0 1em;
       }
     }
+    div.drop-down {
+      position: absolute;
+      display: block;
+      width: 100px;
+      top: 48px;
+      left: 50px;
+      background-color: #fff;
+      box-shadow: 0 2px 6px rgba(0, 0, 0, 0.12);
+      ul {
+        list-style-type: none;
+        padding-inline-start:0px !important;
+        li {
+          display: block;
+          padding: 8px 5px;
+          color: #707070;
+          border-bottom: 1px solid rgba(34, 36, 38, 0.06);
+          :hover {
+            background-color: rgba(34, 36, 38, 0.09);
+            cursor: pointer;
+          }
+          a {
+            color: #707070;
+          }
+        }
+      }
+    }
+    div.drop-down.hidden {
+      display: none;
+    }
+
 `;
 
 const Avatar = styled.div`
@@ -49,14 +83,43 @@ interface Props {
 }
 
 export default function Header2(): any {
+    const [dropDown, setdropDown] = useState(false);
+
+  const container:any = createRef();
+
+  const logoutHandler = () => {
+    store.dispatch(setCurrentUser(''));
+    localStorage.clear();
+  };
+
+  const handleClickOutside = (event:any) => {
+    if (container.current && !container.current.contains(event.target)) {
+      setdropDown(false);
+    }
+  };
+
+  //adding click event to document when mounting header
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  });
     return (
         <Header>
             <img src={Logo2} alt='Journal-Logo'/>
-            <ul className="nav">
+            <ul className="nav" ref={container}>
                 <Avatar/>
                 <div style={{marginTop:'7px',}}>
-                <Icon name='angle down'/>
+                <Icon name='angle down' onClick={() => setdropDown(!dropDown)}/>
+                <div className={`drop-down ${!dropDown ? 'hidden' : null}`}>
+                    <ul onClick={() => setdropDown(false)}>
+                        <Link to="/login" onClick={logoutHandler}>
+                        <li>Log out</li>
+                        </Link>
+                    </ul>
                 </div>
+            </div>
             </ul>
         </Header>
     )
