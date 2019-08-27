99fb03c0477074d62b237c647e0c5f566157a2eb
Benjamin Mark <mquadrant@gmail.com>
2019-08-27 11:05:21 +0100

    added signup and journals to store

--- client/src/store/reducers.tsx
+++ client/src/store/reducers.tsx
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import login from '../containers/Login/redux/reducer';
 import signup from '../containers/Signup/redux/reducer';
+import journals from '../containers/Dashboard/redux/reducer';
 
 const rootReducer = combineReducers({
   login,
-  signup
+  signup,
+  journals
 });
 
 export default rootReducer;
