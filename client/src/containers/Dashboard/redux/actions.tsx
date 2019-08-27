20ed808a2ca45174e97397288748a301b1b0e256
Benjamin Mark <mquadrant@gmail.com>
2019-08-27 11:07:06 +0100

    created actions for journal store

---
+++ client/src/containers/Dashboard/redux/actions.tsx
@@ -0,0 +1,70 @@
+import axios from '../../../utils/API';
+import * as types from './types';
+
+interface IJournal {
+    title:string;
+    body:string
+}
+
+
+export function postJournalAction(payload:any) {
+  return { type: types.SET_POST_JOURNAL, payload};
+}
+
+export function postPending(){
+  return { type: types.POST_JOURNAL_PENDING};
+}
+
+export function postError(error:any) {
+  return { type: types.POST_JOURNAL_ERROR, error };
+}
+
+export const postJournal = (payload:IJournal) => {
+  return (dispatch:any) => { 
+    dispatch(postPending());
+    return (
+      axios
+        .post('/journals', {title:payload.title,
+        body:payload.body})
+        .then(res => {
+          return res;
+        })
+        .catch((error:any) => {
+          dispatch(postError(error));
+          throw error;
+        })
+    );
+  };
+};
+
+
+export function getAllJournalAction(payload:any) {
+    return { type: types.GET_ALL_JOURNAL, payload};
+  }
+  
+  export function getAllPending(){
+    return { type: types.GET_ALL_JOURNAL_PENDING};
+  }
+  
+  export function getAllError(error:any) {
+    return { type: types.GET_ALL_JOURNAL_ERROR, error };
+  }
+
+export const getAllJournals = () => {
+  return (dispatch:any) => { 
+    dispatch(getAllPending());
+    return (
+      axios
+        .get('/journals')
+        .then(res => {
+            console.log(res.data.data)
+            dispatch(getAllJournalAction(res.data.data))
+          return res.data.data;
+        })
+        .catch((error:any) => {
+          dispatch(getAllError(error));
+          throw error;
+        })
+    );
+  };
+};
