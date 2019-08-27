10b593932813e3d3d7a7f2734f01bc0743882ae8
Benjamin Mark <mquadrant@gmail.com>
2019-08-27 11:05:02 +0100

    added bearer to token string

--- client/src/utils/setAuthorizationToken.tsx
+++ client/src/utils/setAuthorizationToken.tsx
@@ -1,9 +1,9 @@
-import axios from 'axios';
+import axios from './API';
 
 // setting token to header
 export default function setAuthorizationToken(token:string) {
   if (token) {
-    axios.defaults.headers.common['Authorization'] = token;
+    axios.defaults.headers.common['Authorization'] = `bearer ${token}`;
   } else {
     delete axios.defaults.headers.common['Authorizaiton'];
   }
